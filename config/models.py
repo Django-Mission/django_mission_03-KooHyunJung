d9c1608efe7a4849f4efc9203292da52c333f247
KooHyunJung <ghj3160@gmail.com>
2022-04-20 10:35:37 +0900

    2차 미션 Basic Mission > Advanced Mission 수정

--- config/models.py
+++ config/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
+# Create your models here.
 
+# 중복 기본 필드 상속 [최종 수정일시, 생성일시] 
+class BaseModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# class BaseModel(models.Model):
-#     updated_at = models.DateTimeField(auto_now=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     class Meta:
-#         abstract = True
+    class Meta:
+        abstract = True
